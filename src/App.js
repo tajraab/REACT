e55c60be804b22074c5a46b21c2e3258a6692be8
tajraab <tajrabacevac@gmail.com>
2023-08-03 14:07:29 +0200

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const customStyle = {
+    color: "blue",
+    fontSize: "20px",
+  };
+  const onClickHandler = () => console.log("console log iz funkcije");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 style={customStyle} onClick={onClickHandler}>
+        Tajra
+      </h1>
+      <button onClick={() => console.log("Tajra")}>Klikni</button>
     </div>
   );
 }
