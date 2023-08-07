36101a60dab7ee2c222033915a5a176bbeead629
tajraab <tajrabacevac@gmail.com>
2023-08-07 11:13:29 +0200

    nastavak vezbe

--- src/components/Cas7avgust.js
+++ src/components/Cas7avgust.js
@@ -1,16 +1,28 @@
 import React, { useState } from "react";
 import "./Cas7avgust.css";
 import Button from "./Button";
-function Cas7avgust() {
-  const [broj, setBroj] = useState(0);
-  return (
-    <div classname="app">
-      <button onClick={() => setBroj(broj + 1)}>Povecati broj</button>
+// function Cas7avgust() {
+//   const [broj, setBroj] = useState(0);
+//   return (
+//     <div classname="app">
+//       <button onClick={() => setBroj(broj + 1)}>Povecati broj</button>
 
-      <button onClick={() => setBroj(broj - 1)}>Smanjiti broj</button>
-      {broj}
+//       <button onClick={() => setBroj(broj - 1)}>Smanjiti broj</button>
+//       {broj}
+//     </div>
+//   );
+// }
+function Cas7avgust() {
+  const [broj, setBroj] = useState([10, 13, 16, 19, 22, 25, 28, 31]);
+  function buttonClicked(broj) {
+    alert(`${broj}ste kliknuli`);
+  }
+  return (
+    <div className="app">
+      {broj.map((el) => {
+        return <Button title={el} onPress={() => buttonClicked(el)} />;
+      })}
     </div>
   );
 }
-
 export default Cas7avgust;
