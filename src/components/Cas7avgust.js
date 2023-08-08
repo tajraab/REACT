ea535f491d2e9d81f9c1d2976c6ceed6681f5c7f
tajraab <tajrabacevac@gmail.com>
2023-08-09 01:07:34 +0200

    vezbanje

--- src/components/Cas7avgust.js
+++ src/components/Cas7avgust.js
@@ -12,17 +12,31 @@ import Button from "./Button";
 //     </div>
 //   );
 // }
+// function Cas7avgust() {
+//   const [broj, setBroj] = useState([10, 13, 16, 19, 22, 25, 28, 31]);
+//   function buttonClicked(broj) {
+//     alert(`${broj}ste kliknuli`);
+//   }
+//   return (
+//     <div className="app">
+//       {broj.map((el) => {
+//         return <Button title={el} onPress={() => buttonClicked(el)} />;
+//       })}
+//     </div>
+//   );
+// }
 function Cas7avgust() {
-  const [broj, setBroj] = useState([10, 13, 16, 19, 22, 25, 28, 31]);
+  const [broj, setBroj] = useState([22, 38, 12, 10, 29, 15]);
   function buttonClicked(broj) {
     alert(`${broj}ste kliknuli`);
   }
   return (
-    <div className="app">
+    <div classname="mapp">
       {broj.map((el) => {
         return <Button title={el} onPress={() => buttonClicked(el)} />;
       })}
     </div>
   );
 }
+
 export default Cas7avgust;
