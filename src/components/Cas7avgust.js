d14fa0a6b26a880dc8f1e8115f40ef43c24b9d44
tajraab <tajrabacevac@gmail.com>
2023-08-09 11:18:08 +0200

    customProducts

--- src/components/Cas7avgust.js
+++ src/components/Cas7avgust.js
@@ -1,22 +1,39 @@
 import React, { useState } from "react";
 import "./Cas7avgust.css";
 import Button from "./Button";
-// function Cas7avgust() {
-//   const [broj, setBroj] = useState(0);
-//   return (
-//     <div classname="app">
-//       <button onClick={() => setBroj(broj + 1)}>Povecati broj</button>
+import { customProducts } from "./data/data";
+import Card from "./Card";
+function Cas7avgust() {
+  //   const [broj, setBroj] = useState(0);
+  //   return (
+  //     <div classname="app">
+  //       <button onClick={() => setBroj(broj + 1)}>Povecati broj</button>
+  //       <button onClick={() => setBroj(broj - 1)}>Smanjiti broj</button>
+  //       {broj}
+  //     </div>
+  //   );
 
-//       <button onClick={() => setBroj(broj - 1)}>Smanjiti broj</button>
-//       {broj}
-//     </div>
-//   );
-// }
-// function Cas7avgust() {
-//   const [broj, setBroj] = useState([10, 13, 16, 19, 22, 25, 28, 31]);
-//   function buttonClicked(broj) {
-//     alert(`${broj}ste kliknuli`);
-//   }
+  // function Cas7avgust() {
+  //     const [allData,]
+  // }
+  //   const [broj, setBroj] = useState([10, 13, 16, 19, 22, 25, 28, 31]);
+
+  //   function buttonClicked(broj) {
+  //     alert(`${broj}ste kliknuli`);
+  //   }
+  console.log(customProducts);
+  return (
+    <div classname="app">
+      {customProducts.map((product) => (
+        <Card
+          title={product.title}
+          description={product.description}
+          price={product.price}
+        />
+      ))}
+    </div>
+  );
+}
 //   return (
 //     <div className="app">
 //       {broj.map((el) => {
@@ -25,18 +42,18 @@ import Button from "./Button";
 //     </div>
 //   );
 // }
-function Cas7avgust() {
-  const [broj, setBroj] = useState([22, 38, 12, 10, 29, 15]);
-  function buttonClicked(broj) {
-    alert(`${broj}ste kliknuli`);
-  }
-  return (
-    <div classname="mapp">
-      {broj.map((el) => {
-        return <Button title={el} onPress={() => buttonClicked(el)} />;
-      })}
-    </div>
-  );
-}
+// function Cas7avgust() {
+//   const [broj, setBroj] = useState([22, 38, 12, 10, 29, 15]);
+//   function buttonClicked(broj) {
+//     alert(`${broj}ste kliknuli`);
+//   }
+//   return (
+//     <div classname="mapp">
+//       {broj.map((el) => {
+//         return <Button title={el} onPress={() => buttonClicked(el)} />;
+//       })}
+//     </div>
+//   );
+// }
 
 export default Cas7avgust;
---
+++ src/components/data/data.js
@@ -0,0 +1,17 @@
+export const customProducts = [
+  {
+    title: "Air Force 1",
+    description: "Old fashion sneakers for every day",
+    price: "110$",
+  },
+  {
+    title: "Air Jordan",
+    description: "Good old Jordan for fresh look",
+    price: "210$",
+  },
+  {
+    title: "Adidas Superstar",
+    description: "Sneakers for every day",
+    price: "100$",
+  },
+];
