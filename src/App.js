1abd832bc57aa1eb1a3cf07c507f5f3e5d96e699
tajraab <tajrabacevac@gmail.com>
2023-08-23 00:26:08 +0200

    vezbaa

--- src/App.js
+++ src/App.js
@@ -5,16 +5,32 @@ import Button from "./components/Button";
 import Cas7avgust from "./components/Cas7avgust";
 
 function App() {
+  const [allData, setAllData] = useState([]);
+  const fetchData = () => { fetch("https://dummyjson.com/products")
+      .then((res) => res.json())
+      .then((someData) => someData.filter((el)=>...........)  )
+     .then((data) => setAllData(data.products));
+   };
+  const[isSwitchOn,setlsSwitchOn]=useState(false)};
+  console.log(isSwitchOn);
   return (
+    <div className="app">
+      {/*{allData.length&&(
+        <div style={{marginTop:"300px",textAlign:"center"}}> 
+<h1>Nema podataka</h1>
+<button onClick={()=>fetchData()}>Fetchaj podatke</button>
+      
+    </div>
+     ) }
     // <div className="app">
     //   <Card marka="Jordan" opis="Duboke,nepromovice patike" cena="12900" />
     //   <Card marka="Nike" opis="Udobne patike,povoljne cene" cena="10200" />
     //   <Card marka="Adidas" opis="Velika povoljnost,garancija zadovoljavajuca" cena="15000" />
     //   <Card marka="Puma" opis="Povoljne patike,visokog kvaliteta" cena="9800" />
     // </div>
-    <div>
-      <Cas7avgust />
-    </div>
+   // <div>
+     // <Cas7avgust />
+   // </div>
   );
 }
 
