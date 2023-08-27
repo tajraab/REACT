4370d7f993ffda19ed4ac3aa27f0c42cb0eeb860
tajraab <tajrabacevac@gmail.com>
2023-08-28 01:07:37 +0200

    vezbe

--- src/App.js
+++ src/App.js
@@ -16,57 +16,88 @@ import Cas7avgust from "./components/Cas7avgust";
 //   return (
 //     <div className="app">
 // {/*{!allData.length &&(
-//         <div style={{marginTop:"300px",textAlign:"center"}}> 
-{/* <h1>Nema podataka</h1>
+//         <div style={{marginTop:"300px",textAlign:"center"}}>
+{
+  /* <h1>Nema podataka</h1>
 <button onClick={()=>fetchData()}>Fetchaj podatke</button>
       
      </div>
-     ) } */}
-    // <div className="app">
-    //   <Card marka="Jordan" opis="Duboke,nepromovice patike" cena="12900" />
-    //   <Card marka="Nike" opis="Udobne patike,povoljne cene" cena="10200" />
-    //   <Card marka="Adidas" opis="Velika povoljnost,garancija zadovoljavajuca" cena="15000" />
-    //   <Card marka="Puma" opis="Povoljne patike,visokog kvaliteta" cena="9800" />
-    // </div>
-   // <div>
-     // <Cas7avgust />
-   // </div>
-  // );
+     ) } */
+}
+// <div className="app">
+//   <Card marka="Jordan" opis="Duboke,nepromovice patike" cena="12900" />
+//   <Card marka="Nike" opis="Udobne patike,povoljne cene" cena="10200" />
+//   <Card marka="Adidas" opis="Velika povoljnost,garancija zadovoljavajuca" cena="15000" />
+//   <Card marka="Puma" opis="Povoljne patike,visokog kvaliteta" cena="9800" />
+// </div>
+// <div>
+// <Cas7avgust />
+// </div>
+// );
 // }
 
-
-
-
 //export default App;
-export default function App(){
+export default function App() {
   //PRVI DOMACI ZADATAK
-  const [allData,setAllData]=useState([]);
-  const fetchData=()=>{
+  const [allData, setAllData] = useState([]);
+  const fetchData = () => {
     fetch("https://dummyjson.com/products")
-     .then((res) => res.json())
-     .then((data)=>{
-      const filteredArray=data.products.filter((el)=>el.category==="smartphones" );
-      setAllData(filteredArray)
-     });
+      .then((res) => res.json())
+      .then((data) => {
+        const filteredArray = data.products.filter(
+          (el) => el.category === "smartphones"
+        );
+        setAllData(filteredArray);
+      });
   };
   console.log(allData);
-  return(
+  return (
     <div>
-      {!allData. length &&(
+      {!allData.length && (
         <div>
           <h1>Nema trenutnih podataka</h1>
-          <button onClick={()=>fetchData()}>Fetchaj podatke</button>
-          </div>
+          <button onClick={() => fetchData()}>Fetchaj podatke</button>
+        </div>
       )}
-      {allData.length?(
-        <div style={{
-          padding:"20px 10px",
-          display:"flex",
-          justifyContent:"space-evenly",
-          flexWrap:"wrap"
-        }}
+      {allData.length ? (
+        <div
+          style={{
+            padding: "20px 10px",
+            display: "flex",
+            justifyContent: "space-evenly",
+            flexWrap: "wrap",
+          }}
         >
-      )}
+          {allData.map((product) => (
+            <Card product={product} />
+          ))}
+        </div>
+      ) : null}
     </div>
-  )
+  );
+  const [randomNumber, setRandomNumber] = useState(0);
+  const genereteRandomNumber = () => {
+    const number = Math.floor(Math.random() * (48 - -36 + 1)) + -36;
+    setRandomNumber(number);
+  };
+  return (
+    <div
+      style={{
+        height: "100vh",
+        width: "100vw",
+        display: "flex",
+        justifyContent: "space-evenly",
+        alignItems: "center",
+        msFlexDirection: "column",
+      }}
+    >
+      <NumberContainer number={randomNumber} />
+      <button
+        onClick={genereteRandomNumber}
+        style={{ width: "30%", height: "10%", backgroundColor: "yellow" }}
+      >
+        Promeni broj
+      </button>
+    </div>
+  );
 }
